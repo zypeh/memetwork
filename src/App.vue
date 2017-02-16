8b38465bbb9673f4c5ea8e99652a9fd18228275a
zypeh <zypeh.geek@gmail.com>
2017-02-16 21:00:08 +0800

    Added footer credits

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id='app'>
+    <header class="header">
+      <router-link to="/">{{ title }}</router-link>
+    </header>
+    <router-view></router-view>
+    <footer class="footer">
+      Copyright © {{ (new Date()).getFullYear() }} |
+      Powered by <a href="https://github.com/zypeh/memetwork" target="_blank">Memetwork</a>
+      Inspired by <a href="https://github.com/viko16/vue-ghpages-blog" target="_blank">vue-ghpages-blog</a>
+    </footer>
   </div>
 </template>
 
@@ -13,3 +22,4 @@ export default {
     return { title: config.blogName, keyword: '' }
   }
 }
+</script>
