c3b250ef23e52e045d5ea266d71c319053e02ab6
zypeh <zypeh.geek@gmail.com>
2017-02-18 01:14:09 +0800

    🖥 (front) AboutView added

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 
 import ListView from '../views/List.vue'
 import PostView from '../views/Post.vue'
+import AboutView from '../views/About.vue'
 
 Vue.use(Router)
 
@@ -11,6 +12,7 @@ export default new Router({
   scrollBehavior: () => ({ y: 0 }),
   routes: [
     { path: '/', name: 'list', component: ListView },
+    { path: '/about', name: 'about', component: AboutView },
     { path: '/post/:hash', name: 'post', component: PostView }
   ]
 })
---
+++ src/views/About.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="about-view">
+    <main class="content" role="main">
+      <article>
+        <div class="page-content about">
+          <h1>About</h1>
+          <h2>Introduction &middot; 自我介绍</h2>
+          <p>🚧⚠️  Working in Progress, coming soon</p>
+          
+          <p>You can always shoot me an email to <a href="mailto: zypeh.geek@gmail.com">zypeh.geek@gmail.com</a> or ping me on <a href="https://twitter.com/zhengyanpeh">Twitter</a>.
+
+          <h3>License &middot; 版权页</h3>
+          <p>This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 👤❌💰📝</p>
+          <p>本站采用 知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议 进行许可。 👤❌💰📝</p>
+        </div>
+      </article>
+    </main>
+  </section>
+</template>
+
+<style lang="scss">
+.about { padding-top: 10vmin; }
+</style>
+
+<script>
+
+export default {
+  name: 'aboutView',
+
+  data() {
+    return { title: '', date: (new Date()) }
+  }
+}
+
+</script>
